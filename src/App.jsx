b78209f3c48ce3503f5b897eb1625698f3e69cee
sankalp-it <31386876+sankalp-it@users.noreply.github.com>
2025-03-10 21:38:42 -0500

    Remove unused imports from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import TwoSectionLayout from './components/TwoSectionLayout';
 import './App.css'
 import {MainContent} from "./components/MainContent";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { TaskList } from "./components/TaskList";
-import Counter from "./components/Counter";
 
 
 export default function App() {
